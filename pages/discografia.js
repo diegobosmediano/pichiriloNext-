60bc18b0a5dbe6efc80d89ddadbfad9aaa80da2e
Diego Bosmediano <diegobosmediano@MacBook-Pro-de-Diego.local>
2020-11-09 16:22:52 -0500

    empezamos aplicacion y primera prueba de musica

---
+++ pages/discografia.js
@@ -0,0 +1,25 @@
+import styles from "../styles/Discografia.module.css"
+
+function Discografia() {
+    return (
+        <div className={styles.marcogeneral}>
+            <div>
+                navegacion
+                
+            </div>
+            <div className={styles.seccionMusica}>
+                social link
+                Imagen escarabajobomba
+                Titulo de cancion: troni
+                controles de musica 
+                
+            </div>
+            <div>
+                logo pichiriolo letras negro
+
+            </div>
+        </div>
+    )
+  }
+  
+  export default Discografia
--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Pichirilo Radioactivo | Official Site</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h3 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </h3>
 
         <p className={styles.description}>
           Get started by editing{' '}
